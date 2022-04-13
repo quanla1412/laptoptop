25602dcd122c818b293ff2a98f242c1490e625fa
thanh <tuanthanh15112002@gmail.com>
2022-04-13 23:09:12 +0700

    thanh hoàn thành liên hệ

---
+++ lienhe.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://kit.fontawesome.com/26a480acf4.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/style.css">
+    
+    <title>Laptoptop</title>
+</head>
+<body>
+<div class="container_swap">
+
+<div class="div_right" ><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.6697269378988!2d106.68006961442788!3d10.759917092332769!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f1b7c3ed289%3A0xa06651894598e488!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBTw6BpIEfDsm4!5e0!3m2!1svi!2s!4v1649862985682!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+<div class="div_left" id="contact_info" class="divdetail"><strong>Cơ Sở Chính </strong><br>
+Địa chỉ : 273, An Dương Vương, P.3, Q.5, Tp.Hồ Chí Minh<br>
+Điện Thoại: <strong><a href="tel:0327658521" title="0327 658 521">0327658521</a></strong>&nbsp;<br>
+Email: tuanthanh15112002@gmail.com<br>
+<br>
+<strong>Cơ Sở 1</strong><br>
+Địa chỉ : 105, Bà Huyện Thanh Quan, Q.3, Tp.Hồ Chí Minh<br>
+Điện Thoại: <strong><a href="tel:0327658521" title="0327 658 521">0327658521</a></strong>&nbsp;<br>
+Email: tuanthanh15112002@gmail.com<br>
+<br>
+<strong>Cơ Sở 2</strong><br>
+Địa chỉ : 1A, Nguyễn Hữu Cảnh, P.Bến Nghé, Q.1, Tp.Hồ Chí Minh<br>
+Điện Thoại: <strong><a href="tel:0327658521" title="0327 658 521">0327658521</a></strong>&nbsp;<br>
+Email: tuanthanh15112002@gmail.com<br>
+<br>
+</div>
+</div>
+    <script src="https://kit.fontawesome.com/26a480acf4.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+</html>
+
+
+
