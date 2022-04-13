2ce0041368d359f462365d0c2fededbbaa6e2061
Anh Quan <anhquanle2015@gmail.com>
2022-04-13 21:02:25 +0700

    bổ sung header động - hoàn thành page khuyến mãi

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <script src="https://kit.fontawesome.com/26a480acf4.js" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/style.css">
-    
-    <title>Laptoptop</title>
-</head>
-<body>
+<?php include "./header.php"?>
     <div class="slider container-xl" >
         <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner" style="height:400px">
@@ -114,11 +101,76 @@
                     </div>
                 </div>
             </div>
-            <div class="card-footer text-muted"></div>
+            <div class="card-footer text-muted" style="background-color: var(--third-color);"></div>
         </div>
     </div>
 
-    <script src="https://kit.fontawesome.com/26a480acf4.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
+    
+    <!-- Content 2 -->
+    <div class="content1 container-xl mt-5">
+        <div class="card text-center">
+            <div class="card-header fw-bold fs-4" style="background-color: var(--third-color); color: white;">Loại sản phẩm</div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="./assets/image/square-sale/sale1.png" alt="itemsale1" class="w-100">
+                    </div>
+                    <div class="col-9">
+                        <div class="row">
+                            <div class="col-3">
+                                <div class="card" style="width: 100%;">
+                                    <img src="./assets/image/square-sale/sale1.png" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Acer Nitro 5</h5>
+                                        <p class="card-text">23.125.000 VND</p>
+                                        <a href="#" class="btn btn-primary" style="background-color: var(--third-color)">Mua ngay</a>
+                                    </div>    
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card" style="width: 100%;">
+                                    <img src="./assets/image/square-sale/sale1.png" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Acer Nitro 5</h5>
+                                        <p class="card-text">23.125.000 VND</p>
+                                        <a href="#" class="btn btn-primary" style="background-color: var(--third-color)">Mua ngay</a>
+                                    </div>    
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card" style="width: 100%;">
+                                    <img src="./assets/image/square-sale/sale1.png" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Acer Nitro 5</h5>
+                                        <p class="card-text">23.125.000 VND</p>
+                                        <a href="#" class="btn btn-primary" style="background-color: var(--third-color)">Mua ngay</a>
+                                    </div>    
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card" style="width: 100%;">
+                                    <img src="./assets/image/square-sale/sale1.png" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Acer Nitro 5</h5>
+                                        <p class="card-text">23.125.000 VND</p>
+                                        <a href="#" class="btn btn-primary" style="background-color: var(--third-color)">Mua ngay</a>
+                                    </div>    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3 mx-1">
+                            <div class="nav col">                                
+                                <button type="button" class="btn btn-outline-secondary me-2"><i class="fa-solid fa-angles-left"></i></button>
+                                <button type="button" class="btn btn-outline-secondary"><i class="fa-solid fa-angles-right"></i></button>
+                            </div>
+                            <div class="nav-more col pe-0">
+                                <button type="button" class="btn btn-outline-secondary float-end" >Xem thêm</i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer text-muted" style="background-color: var(--third-color);"></div>
+        </div>
+    </div>
 <?php include "./footer.php" ?>
---
+++ sale.php
@@ -0,0 +1,44 @@
+<?php include "./header.php" ?>
+
+<div class="container-xl">
+    <div class="card mt-5">
+        <div class="card-body">
+            <h5 class="card-title">GEAR GIÁ SỐC - GIAO SIÊU TỐC - ƯU ĐÃI ĐẾN 51% - FREESHIP TOÀN QUỐC</h5>
+            <!-- <p class="card-text"></p> -->
+            <p class="card-text"><small class="text-muted">13/4/2022</small></p>
+        </div>
+        <img src="./assets/image/sale-page/event1.png" class="card-img-bottom" alt="event1">
+    </div>
+    
+    <div class="card mt-5">
+        <div class="card-body">
+            <h5 class="card-title">TỔNG HỢP KHUYẾN MÃI CHUỘT GAMING</h5>
+            <!-- <p class="card-text"></p> -->
+            <p class="card-text"><small class="text-muted">13/4/2022</small></p>
+        </div>
+        <img src="./assets/image/sale-page/event2.png" class="card-img-bottom" alt="event2">
+    </div>
+
+    <div class="d-flex justify-content-center mt-5">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
+
+
+<?php include "./footer.php" ?>
