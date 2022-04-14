de5c4d5ac35be072ba58d7b39176789b68887641
Anh Quan <anhquanle2015@gmail.com>
2022-04-14 12:18:40 +0700

    basic chi tiet san pham

---
+++ product.php
@@ -0,0 +1,18 @@
+<?php include "./header.php" ?>
+
+<div class="container-xl">
+    <div class="row">
+        <div class="col-6">
+            <div class="row">
+                <div class="container-fluid p-3" style="background-color: var(--gray-color)">
+                    <img src="./assets/image/product/item1.png" alt="item1" class="w-100">
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+
+        </div>
+    </div>
+</div>
+
+<?php include "./footer.php" ?>
