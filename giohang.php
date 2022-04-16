1cb3497cf54b1ad8f9d59ff6d3d812f4104b3d16
thanh <tuanthanh15112002@gmail.com>
2022-04-16 22:19:02 +0700

    xong giỏ hàng

---
+++ giohang.php
@@ -0,0 +1,72 @@
+<?php include "./header.php"?>
+<div class="container">
+  <div class="row align-items-start">
+    <div class="col-12  mb-4 mt-5 pt-1 pb-1" style="background-color: #E5E5E5;font-weight: bold; border-radius: 20px; font-size: 24px;">
+      Giỏ Hàng
+    </div>
+  </div>
+  <div class="row mt-0">
+        <div class="col-8">
+            <div class="row  " style="background-color: #E5E5E5; height: 200px; border-radius: 20px;">
+                <div class="col-3"><img src="./assets/image/square-sale/sale1.png" alt="itemsale1" class="w-100 mt-3 ms-2"></div>
+                <div class="col-5 mt-3">
+                    <h4 class="mb-0">Acer Nitro 5</h4><br>
+                    <h6 class="mb-0">Mã máy</h6><br>
+                    <h5 class="mb-0">Cấu hình máy</h5>
+                </div>
+                <div class="col-2">
+                <div class="input_qty_pr mt-3"><input class="variantID" type="hidden" name="variantId" value="63233014"><button class="border rounded" onclick="var result = document.getElementById('qtyItem63233014'); var qtyItem63233014 = result.value; if( !isNaN( qtyItem63233014 ) &amp;&amp; qtyItem63233014 > 1 ) result.value--;return false;" class="reduced_pop items-count btn-minus" type="button">–</button><input class="border rounded" style="text-align: center;" type="text" maxlength="12" min="1" max= "10" class="input-text number-sidebar input_pop input_pop qtyItem63233014" id="qtyItem63233014" name="Lines" size="1" value="1"><button class="border rounded" onclick="var result = document.getElementById('qtyItem63233014'); var qtyItem63233014 = result.value; if( !isNaN( qtyItem63233014 )) result.value++;return false;" class="increase_pop items-count btn-plus" type="button">+</button></div>
+                </div>
+                <div class="col-2">
+                    <h6 class="mt-3 pt-1 mb-0" style="color: #1E656D">22.000.000</h6><br>
+                    <button type="button" class="btn btn-outline-dark border-0 ps-4 pe-4 pt-0 pb-0 bg-white"style="font-weight: bold">Xóa</button>
+                </div>
+            </div> 
+            <div class="row  mt-1" style="background-color: #E5E5E5; height: 200px; border-radius: 20px;">
+            <div class="col-3"><img src="./assets/image/square-sale/sale1.png" alt="itemsale1" class="w-100 mt-3 ms-2"></div>
+            <div class="col-5 mt-3">
+                    <h4 class="mb-0">Acer Nitro 5</h4><br>
+                    <h6 class="mb-0">Mã máy</h6><br>
+                    <h5 class="mb-0">Cấu hình máy</h5>
+                </div>
+                <div class="col-2">
+                <div class="input_qty_pr mt-3"><input class="variantID" type="hidden" name="variantId" value="63233014"><button class="border rounded" onclick="var result = document.getElementById('qtyItem63233014'); var qtyItem63233014 = result.value; if( !isNaN( qtyItem63233014 ) &amp;&amp; qtyItem63233014 > 1 ) result.value--;return false;" class="reduced_pop items-count btn-minus" type="button">–</button><input class="border rounded" style="text-align: center;" type="text" maxlength="12" min="1" max= "10" class="input-text number-sidebar input_pop input_pop qtyItem63233014" id="qtyItem63233014" name="Lines" size="1" value="1"><button class="border rounded" onclick="var result = document.getElementById('qtyItem63233014'); var qtyItem63233014 = result.value; if( !isNaN( qtyItem63233014 )) result.value++;return false;" class="increase_pop items-count btn-plus" type="button">+</button></div>
+                </div>
+                <div class="col-2">
+                    <h6 class="mt-3 pt-1 mb-0" style="color: #1E656D">22.000.000</h6><br>
+                    <button type="button" class="btn btn-outline-dark border-0 ps-4 pe-4 pt-0 pb-0 bg-white"style="font-weight: bold">Xóa</button>
+                </div>
+            </div>
+            <div class="row  mt-1" style="background-color: #E5E5E5; height: 200px; border-radius: 20px;">
+            <div class="col-3"><img src="./assets/image/square-sale/sale1.png" alt="itemsale1" class="w-100 mt-3 ms-2"></div>
+            <div class="col-5 mt-3">
+                    <h4 class="mb-0">Acer Nitro 5</h4><br>
+                    <h6 class="mb-0">Mã máy</h6><br>
+                    <h5 class="mb-0">Cấu hình máy</h5>
+                </div>
+                <div class="col-2">
+                <div class="input_qty_pr mt-3"><input class="variantID" type="hidden" name="variantId" value="63233014"><button class="border rounded" onclick="var result = document.getElementById('qtyItem63233014'); var qtyItem63233014 = result.value; if( !isNaN( qtyItem63233014 ) &amp;&amp; qtyItem63233014 > 1 ) result.value--;return false;" class="reduced_pop items-count btn-minus" type="button">–</button><input class="border rounded" style="text-align: center;" type="text" maxlength="12" min="1" max= "10" class="input-text number-sidebar input_pop input_pop qtyItem63233014" id="qtyItem63233014" name="Lines" size="1" value="1"><button class="border rounded" onclick="var result = document.getElementById('qtyItem63233014'); var qtyItem63233014 = result.value; if( !isNaN( qtyItem63233014 )) result.value++;return false;" class="increase_pop items-count btn-plus" type="button">+</button></div>
+                </div>
+                <div class="col-2">
+                    <h6 class="mt-3 pt-1 mb-0" style="color: #1E656D">22.000.000</h6><br>
+                    <button type="button" class="btn btn-outline-dark border-0 ps-4 pe-4 pt-0 pb-0 bg-white"style="font-weight: bold">Xóa</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-4" >
+            <div class="mt-0" style="background-color: #E5E5E5;height: 166px;border-radius: 20px;">
+                <h5 class="pt-2 ps-3">Thông Tin Thanh Toán</h5>
+                <h6 class="mt-4 mb-2 ps-3">
+                    <h6 class="ps-3" style="float: left;">Tạm tính</h6>
+                    <h6 class="pe-3" style="text-align: right;">66.000.000 </h6>
+                    <h6 class="ps-3" style="float: left;">Thành tiền</h6>
+                    <h6 class="pe-3" style="text-align: right;color: #1E656D; font-size: 24px;">66.000.000 </h6>
+                </h6>
+                <div class="d-grid gap-2 mt-2">
+                    <button class="btn btn-outline-primary border-0 ms-3 me-3" type="button" style="background-color: #1E656D; color: white">Đặt Hàng</button>
+                </div>
+            </div>
+        </div>
+  </div>
+</div>
+<?php include "./footer.php" ?>
