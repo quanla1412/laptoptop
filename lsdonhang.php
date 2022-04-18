ae145fae162a1dc7a06d110b63547de8de23962e
thanh <tuanthanh15112002@gmail.com>
2022-04-18 14:56:19 +0700

    50% ls đơn hàng

---
+++ lsdonhang.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://kit.fontawesome.com/26a480acf4.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/style.css">
+    
+    <title>Laptoptop</title>
+</head>
+<body>
+<div class="container">
+  <div class="row">
+            <div class="row pt-2 pb-2" style="background-color: #E5E5E5;border-radius: 20px;font-weight: bold; font-size: 24px;">
+                Lịch Sử Đơn Hàng
+            </div>
+            <div class="row mt-4 " style="background-color: #E5E5E5; border-radius: 20px;">
+                <div class="row"><h4>Đơn Hàng: Mã Đơn</h4></div>
+                <div class="row">
+                    <div class="col-3"><img src="./assets/image/square-sale/sale1.png" alt="itemsale1" class="w-100 mt-3 ms-2"></div>
+                    <div class="col-8 mt-3">
+                        <h4 class="">Acer Nitro 5</h4><br>
+                        <h6 class="">Mã máy</h6><br>
+                        <h5 class="">Cấu hình máy</h5>
+                        <h6 class="row ms-2 mt-5 pt-4">Số lượng: 1</h6>
+                    </div>
+                    <div class="col-1 align-items-end">
+                        <h6 class="mt-5 pt-5 mb-0" style="color: #1E656D">22.000.000</h6><br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3"><img src="./assets/image/square-sale/sale1.png" alt="itemsale1" class="w-100 mt-3 ms-2"></div>
+                    <div class="col-8 mt-3">
+                        <h4 class="">Acer Nitro 5</h4><br>
+                        <h6 class="">Mã máy</h6><br>
+                        <h5 class="">Cấu hình máy</h5>
+                        <h6 class="row ms-2 mt-5 pt-4">Số lượng: 1</h6>
+                    </div>
+                    <div class="col-1 align-items-end">
+                        <h6 class="mt-5 pt-5 mb-0" style="color: #1E656D">22.000.000</h6><br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3"><img src="./assets/image/square-sale/sale1.png" alt="itemsale1" class="w-100 mt-3 ms-2"></div>
+                    <div class="col-8 mt-3">
+                        <h4 class="">Acer Nitro 5</h4><br>
+                        <h6 class="">Mã máy</h6><br>
+                        <h5 class="">Cấu hình máy</h5>
+                        <h6 class="row ms-2 mt-5 pt-4">Số lượng: 1</h6>
+                    </div>
+                    <div class="col-1 align-items-end">
+                        <h6 class="mt-5 pt-5 mb-0" style="color: #1E656D">22.000.000</h6><br>
+                    </div>
+                </div>
+                <div class="row">
+                    <h6 class="pe-3" style="text-align: right;color: #1E656D; font-size: 24px;">
+                        <p style="color: #000;">Tổng tiền: </p>
+                        66.000.000 
+                    </h6>
+                </div>
+            </div>
+            <div class="row mt-4 " style="background-color: #E5E5E5; height: 200px; border-radius: 20px;">
+            </div>
+  </div>
+</div>
+    <script src="https://kit.fontawesome.com/26a480acf4.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+</html>
