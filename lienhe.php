75d987bbd815385f5a5e619c9ee37c84d200418a
thanh <tuanthanh15112002@gmail.com>
2022-04-18 17:02:06 +0700

    hoàng thành ls đơn hàng

--- lienhe.php
+++ lienhe.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <script src="https://kit.fontawesome.com/26a480acf4.js" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/style.css">
-    
-    <title>Laptoptop</title>
-</head>
-<body>
+<?php include "./header.php"?>
 <div class="container_swap">
 
 <div class="div_right" ><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.6697269378988!2d106.68006961442788!3d10.759917092332769!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f1b7c3ed289%3A0xa06651894598e488!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBTw6BpIEfDsm4!5e0!3m2!1svi!2s!4v1649862985682!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
@@ -32,10 +19,7 @@ Email: tuanthanh15112002@gmail.com<br>
 <br>
 </div>
 </div>
-    <script src="https://kit.fontawesome.com/26a480acf4.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    </body>
-</html>
+<?php include "./footer.php" ?>
 
 
 
