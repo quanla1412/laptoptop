0b7ebcf72b1f5f933231e57e24bad5b1cbeade22
thanh <tuanthanh15112002@gmail.com>
2022-05-18 00:33:24 +0700

    php đăng nhập đăng kí

--- login.php
+++ login.php
@@ -1,11 +1,44 @@
-<?php include "./header.php" ?>
+<?php include "./header.php"?>
 
 <div class="d-flex justify-content-center">
     <div class="border border-dark p-3" style="width: 800px; background-color: var(--third-color)">
         <p class="h5 mb-3 text-white">Đăng nhập</p>
-        <input type="text" class="form-control mb-3" placeholder="Nhập tên đăng nhập">
-        <input type="text" class="form-control mb-3" placeholder="Nhập mật khẩu">
-        <button type="button" class="btn mb-3" style="border:none; background-color: var(--second-color); color: var(--dark-color)">Đăng nhập</button>
+        <form action="login.php" method="POST">
+        <input type="text" class="form-control mb-3" placeholder="Nhập tên đăng nhập" name="username" required>
+        <input type="password" class="form-control mb-3" placeholder="Nhập mật khẩu" name="password" required>
+        <input type="submit" class="btn mb-3" style="border:none; background-color: var(--second-color); color: var(--dark-color)" value="Đăng Nhập">
+        </form>
+        <?php
+                $servername = "localhost";
+                $username = "laptoptop";
+                $password = "laptoptop";
+                $dbname = "laptoptop";
+        
+        
+                $conn = new mysqli($servername, $username, $password, $dbname);
+            if(isset($_POST['username'])){
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+        
+                $sql= "SELECT * FROM taikhoan WHERE TenDangNhap = '$username' ";
+                $result = $conn->query($sql);
+        
+                if ($result->num_rows > 0) {
+
+                    $sql= "SELECT * FROM taikhoan WHERE Matkhau = '$password' ";
+                    $result = $conn->query($sql);
+                    if($result->num_rows == true){
+                        echo '<meta http-equiv="refresh" content="0;url=index.php">';
+                    }
+                    else{
+                        echo '<h6 class="text-danger">Mật Khẩu Không Chính Xác</h6>';
+                    }
+                }
+                else {
+                    echo '<h6 class="text-danger">Tên Đăng Nhập Không Chính Xác</h6>'; 
+                }
+            }
+        ?>
         <p class="text-white ">Bạn chưa có tài khoản? <a href="./register.php" class="link-primary">Đăng ký</a></p>
     </div>
 </div>
--- register.php
+++ register.php
@@ -3,14 +3,50 @@
 <div class="d-flex justify-content-center">
     <div class="border border-dark p-3" style="width: 800px; background-color: var(--third-color)">
         <p class="h5 mb-3 text-white">Đăng ký</p>
-        <input type="text" class="form-control mb-3" placeholder="Họ và tên">
-        <input type="text" class="form-control mb-3" placeholder="Số điện thoại">
-        <input type="date" class="form-control mb-3">
-        <input type="email" class="form-control mb-3" placeholder="Email">
-        <input type="text" class="form-control mb-3" placeholder="Tên đăng nhập">
-        <input type="password" class="form-control mb-3" placeholder="Nhập mật khẩu">
-        <input type="password" class="form-control mb-3" placeholder="Nhập lại mật khẩu">
-        <button type="button" class="btn mb-3" style="border:none; background-color: var(--second-color); color: var(--dark-color)">Đăng ký</button>
+        <form action="register.php" method="POST">
+        <input type="text" class="form-control mb-3" placeholder="Họ và tên" name="hoten" required>
+        <input type="tel" class="form-control mb-3" placeholder="Số điện thoại" name="sdt" pattern="[0][0-9]{9}" required>
+        <input type="date" class="form-control mb-3" name="" required>
+        <input type="email" class="form-control mb-3" placeholder="Email" name="email" pattern= "[a-z0-9]+@[a-z0-9]+\.[a-z]{2,4}$" required>
+        <input type="text" class="form-control mb-3" placeholder="Tên đăng nhập" name="username" required>
+        <input type="password" class="form-control mb-3" placeholder="Nhập mật khẩu" name="password" required>
+        <input type="password" class="form-control mb-3" placeholder="Nhập lại mật khẩu" name="repassword" required>
+        <input type="submit" class="btn mb-3" style="border:none; background-color: var(--second-color); color: var(--dark-color)" value="Đăng Kí">
+        </form>
+        <?php
+                $servername = "localhost";
+                $username = "laptoptop";
+                $password = "laptoptop";
+                $dbname = "laptoptop";
+        
+        
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                if ($_SERVER['REQUEST_METHOD'] == 'POST')
+                {
+                    $username = $_POST['username'];
+                    $password = $_POST['password'];
+
+                    $sql= "SELECT * FROM taikhoan WHERE TenDangNhap = '$username' ";
+                    $result = $conn->query($sql);
+        
+                    if ($result->num_rows > 0) {
+                        echo '<h6 class="text-danger">Tên Đăng Nhập Đã Được Sử Dụng</h6>';
+                    }
+                    else { 
+                    
+                        if($_POST['password'] == $_POST['repassword'])
+                        {
+                            $sql= "INSERT INTO TaiKhoan(TenDangNhap,MatKhau,LoaiTK) VALUES ('$username','$password','us')";
+                            $result = $conn->query($sql);
+                            echo '<h6 class="text-white">Đăng Kí Thành công</h6>';
+                            echo '<meta http-equiv="refresh" content="0;url=login.php">';
+                        }
+                        else{
+                            echo '<h6 class="text-danger">Nhập Lại Mật Khẩu Không Chính Xác</h6>';
+                        }
+                    }
+                }
+        ?>
         <p class="text-white ">Bạn đã có tài khoản? <a href="./login.php" class="link-primary">Đăng nhập</a></p>
     </div>
 </div>
