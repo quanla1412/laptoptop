851016f2eb4d005af9b71c86a265af654b19a7b6
thanh <tuanthanh15112002@gmail.com>
2022-05-25 02:44:45 +0700

    lỗi tìm sp

--- collections.php
+++ collections.php
@@ -404,6 +404,13 @@
                         }
                     }
                 }
+                if($soTrang==0) {
+                    echo '<div class="p-5 mt-5">
+                    <div class="d-flex justify-content-center">
+                        <img class="" alt="" src="https://firebasestorage.googleapis.com/v0/b/mongcaifood.appspot.com/o/no-products-found.png?alt=media&amp;token=2f22ae28-6d48-49a7-a36b-e1a696618f9c" loading="lazy" decoding="async">
+                    </div> <br>
+                    <div class="d-flex justify-content-center">Không tìm thấy sản phẩm nào</div>
+                </div> ';}
                 if($soTrang>1) {
                     echo '<li class="page-item ';
                     if($trangHienTai<=1) echo 'disabled';
